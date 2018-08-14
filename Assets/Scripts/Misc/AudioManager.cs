21ad2aae982f837e66c8ba78542bfaa0e1447471
Hayden Donnelly <Bizbud@live.com>
2018-08-13 21:17:59 -0400

    Edits

---
+++ Assets/Scripts/Misc/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour 
+{
+	public static AudioManager instance;
+	[SerializeField] private AudioSource source;
+
+	private void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		source.Play();
+	}
+}
